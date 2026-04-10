012366512c40465d59c11b19dba39688248d9dcc
MΛX <maximiliano.pina@mail.udp.cl>
2026-04-09 20:45:02 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+//ejercicio 01 MAXPIÑA 20/03
+function setup() {
+  createCanvas(400, 500);
+background(200);
+  stroke(0,0,0);
+  strokeWeight(5);
+} 
+function draw(){
+  
+triangle(0,50,400,50,200,450);  
+  
+  ellipse(400/5,100,120,50);
+  circle(400/5,100,50)
+    ellipse(400/5,175,120,50);
+    circle(400/5,175,50)
+ellipse(400/5,250,120,50);
+  circle(400/5,250,50)
+  
+  ellipse(320,250,120,50);
+    circle(320,250,50)
+
+    ellipse(320,175,120,50);
+      circle(320,175,50)
+
+    ellipse(320,100,120,50);
+      circle(320,100,50)
+
+    ellipse(320,325,120,50);
+      circle(320,325,50)
+  
+  line(200,50,200,450)
+  
+}  
+ 
+
